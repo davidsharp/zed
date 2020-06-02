cdfb32d900fa938c7f40fdad7750c3f7bfdb43c2
davidsharp <hello@davidsharp.codes>
2020-06-02 12:11:52 +0100

    Crib broken show/hide hidden file aliases out and fix

---
+++ scripts/macos.sh
@@ -0,0 +1,19 @@
+# Assorted macOS specific functions
+
+# toggle hidden files, source: https://ianlunn.co.uk/articles/quickly-showhide-hidden-files-mac-os-x-mavericks/
+# showHiddens and hideHiddens implicitly kill Finder
+alias showHiddens='defaults write com.apple.finder AppleShowAllFiles YES; killall Finder /System/Library/CoreServices/Finder.app'
+alias hideHiddens='defaults write com.apple.finder AppleShowAllFiles NO; killall Finder /System/Library/CoreServices/Finder.app'
+# showDots and hideDots prompt for killing
+alias showDots='defaults write com.apple.finder AppleShowAllFiles YES; finderKillPrompt'
+alias hideDots='defaults write com.apple.finder AppleShowAllFiles NO; finderKillPrompt'
+function finderKillPrompt(){
+  read 'kill?Kill Finder? Y to kill, then hit [ENTER]: '
+  if [[ $kill =~ "^(y|Y|yes|YES|Yes)$" ]];
+  then
+    echo 'Killing Finder';
+    killall Finder /System/Library/CoreServices/Finder.app;
+  else
+    echo 'Finder lives to find another day';
+  fi
+}
--- scripts/wildwest.sh
+++ scripts/wildwest.sh
@@ -29,26 +29,7 @@ function fork(){
 }
 
 # cd to a directory and ls immediately
-function cdls(){ builtin cd "$@"; ls -FGAhno; }  
-
-# toggle hidden files, source: https://ianlunn.co.uk/articles/quickly-showhide-hidden-files-mac-os-x-mavericks/
-# showHiddens and hideHiddens implicitly kill Finder
-alias showHiddens='defaults write com.apple.finder AppleShowAllFiles YES; killall Finder /System/Library/CoreServices/Finder.app'
-alias hideHiddens='defaults write com.apple.finder AppleShowAllFiles NO; killall Finder /System/Library/CoreServices/Finder.app'
-# showDots and hideDots prompt for killing
-alias showDots='defaults write com.apple.finder AppleShowAllFiles YES; finderKillPrompt'
-alias hideDots='defaults write com.apple.finder AppleShowAllFiles NO; finderKillPrompt'
-function finderKillPrompt(){
-  echo 'Kill Finder? Y to kill, then hit [ENTER]: '
-  read kill
-  if [[ $kill =~ ^(y|Y|yes|YES|Yes)$ ]];
-  then
-    echo 'Killing Finder';
-    killall Finder /System/Library/CoreServices/Finder.app;
-  else
-    echo 'Finder lives to find another day';
-  fi
-}
+function cdls(){ builtin cd "$@"; ls -FGAhno; }
 
 # grab current weather takes an argument for the place
 # `$ wttr brighton` or `$ wttr moon` or `$ wttr yyz` or even `$ wttr :help`
